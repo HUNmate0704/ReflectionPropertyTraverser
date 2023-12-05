50ee885a25079cd214ec138ddffc9eff731ca620
Darker Laptop <hunmate0704@gmail.com>
2023-12-05 16:10:05 +0100

    Normal, collection traversers (On properties with reflection)

--- ReflectionPropertyTraverser/Program.cs
+++ ReflectionPropertyTraverser/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ReflectionPropertyTraverser
 {
@@ -6,7 +7,57 @@ namespace ReflectionPropertyTraverser
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //DateTime now = DateTime.Now;
+            //Traverser(now);
+
+            List<DateTime> dates = new List<DateTime>()
+            {
+                DateTime.Now.AddYears(1),
+                DateTime.Now.AddYears(2),
+                DateTime.Now.AddYears(3),
+            };
+
+            CollectionTraverser(dates);
+
         }
+
+        static void Traverser(object o)
+        {
+            Type t = o.GetType();
+            var props = t.GetProperties();
+            foreach (var item in props)
+            {
+                Console.WriteLine(item.Name + " : " + item.GetValue(o));
+            }
+
+        }
+
+        static void CollectionTraverser<T>(IEnumerable<T> collection)
+        {
+            Type t = typeof(T);
+            foreach (var item in t.GetProperties())
+            {
+                Console.Write(item.Name.Length > 7 ? item.Name.Substring(0,7) : item.Name + "\t");
+
+            }
+            Console.WriteLine();
+            foreach (var item in collection)
+            {
+                foreach (var prop in t.GetProperties())
+                {
+                    if (prop.GetValue(item).ToString().Length > 7)
+                    {
+                        Console.Write(prop.GetValue(item).ToString().Substring(0,7) + "\t");
+                    }
+                    else
+                    {
+                        Console.Write(prop.GetValue(item) + "\t");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
     }
 }
